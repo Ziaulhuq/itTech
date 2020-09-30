8c524b1765340163bb7cb7d6a731dfabb70dc9ad
ziaulhuq <ziaulhuqbe@gmail.com>
2020-09-30 16:44:57 +0800

    files uploading

---
+++ app.js
@@ -0,0 +1,24 @@
+window.sr = ScrollReveal();
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "6rem",
+  delay: 300,
+  reset: true,
+});
+
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "6rem",
+  delay: 300,
+  reset: true,
+});
+
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "6rem",
+  delay: 300,
+  reset: true,
+});
